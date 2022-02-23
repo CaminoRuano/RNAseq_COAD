9c2e291995df9add2d2c396dc73fa49e50a0fd41
CaminoR <75613356+CaminoRuano@users.noreply.github.com>
2022-02-23 15:27:10 +0100

    Update RNA_seq_pipeline.R

--- RNA_seq_pipeline.R
+++ RNA_seq_pipeline.R
@@ -1,103 +1,37 @@
 
 
-# RNAseq Analysis on Rstudio
+### main DESeq (normalize counts for gene lenght and depth of read)
+  ### log2 tables + Wald test for p-value
+  ### comparison with the LAST variable of the design : "race"
+  ### reference level = 
+
+data = DESeq(data, test=c("Wald"))
+
+
+########### check results
+
+normCounts <-  counts(data, normalized=T)
+View(normCounts)
+
+res <- DESeq2::results(data, alpha=0.05)
+
+summary(res)
+
+# order by adj.p.value
+res = res[order(res$padj),]
+
+##annotation
 
 
 
-## RNAseq Analysis on Rstudio
-
-setwd("C://Users/Camino/Documents/EpigeneLabs/")
-
-#library import 
-
-library("TCGAbiolinks")
-library("limma")
-library("edgeR")
-library("glmnet")
-library("factoextra")
-library("FactoMineR")
-library("caret")
-library("SummarizedExperiment")
-library("gplots")
-library("survival")
-library("survminer")
-library("RColorBrewer")
-library("gProfileR")
-library("genefilter")
-library("DESeq2")
-library("TCGAutils")
-library('GenomicDataCommons')
-library('dplyr')
-
-### Download the data ###
-
-GDCprojects = getGDCprojects()
-
-head(GDCprojects[c("project_id", "name")])
-
-TCGAbiolinks:::getProjectSummary("TCGA-COAD")
+### is ethnicity/sex/age involved in the question
 
 
-  ### No metastasis data
-query_TCGA = GDCquery(
-  project = "TCGA-COAD",
-  data.category = "Transcriptome Profiling", # parameter enforced by GDCquery
-  experimental.strategy = "RNA-Seq",
-  workflow.type = "HTSeq - Counts", sample_type=("Primary Tumor", "Solid Tissue Normal"))
+##### DESeq2 normalization
 
 
-lihc_res = getResults(query_TCGA)
-colnames(lihc_res)
-
-GDCdownload(query = query_TCGA)
-tcga_data = GDCprepare(query_TCGA)
-dim(tcga_data)
-
-colnames(colData(tcga_data))
-table(tcga_data@colData$tissue_or_organ_of_origin)
-
-head(rowData(tcga_data))
-
-saveRDS(object = tcga_data,
-        file = "./tcga_data.RDS",
-        compress = FALSE)
-
-
-## Metadata file = match file names with case name from clinical data txt
-
-
-# file names
-
-sample_file = read.csv("./metadata/gdc_sample_sheet.2022-02-19.tsv", sep="\t", header=TRUE)
-sample_file = sample_file[sample_file$Sample.Type=="Primary Tumor",]
-uniq = unique(sample_file$Case.ID)   # check the n° of unique cases - sometimes there are samples with several sequences or samples with tumour and solid normal samples-
-
-sample_file <- as.data.frame(sample_file %>% dplyr::group_by(Case.ID) %>% dplyr::filter (! duplicated(Case.ID)))
-
-# clinical data
-
-clinical_data = read.csv("./metadata/clinical.tsv", sep="\t", header=TRUE)
-clinical_data = clinical_data[clinical_data$treatment_type=="Radiation Therapy, NOS",]
-clinical_data = dplyr::filter(clinical_data, clinical_data$case_submitter_id %in% sample_file$Case.ID)
-
-clinical_data <- as.data.frame(clinical_data %>% dplyr::group_by(case_submitter_id) %>% dplyr::filter (! duplicated(case_submitter_id)))
-
-
-sample_file = dplyr::filter(sample_file, sample_file$Case.ID %in% clinical_data$case_submitter_id)
-
-# merge both files together
-rownames(sample_file) = sample_file$Case.ID
-rownames(clinical_data)=clinical_data$case_submitter_id
-
-
-metadata = cbind(sample_file, clinical_data)
+featureData <- data.frame(gene=rownames(data))
 
 
 
-### Create DESeq file for normalization
 
-data = DESeqDataSetFromHTSeqCount(sampleTable = metadata, directory = "./GDCdata", design ~ Sample.Type)
-
-
-
-#### matching Case.UUIDs to File.UUIDs
